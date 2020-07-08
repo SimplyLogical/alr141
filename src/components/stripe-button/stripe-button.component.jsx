f47326d684c8394b5cdeb811849d90ba09e1d2e5
Sean Allen <alle3605@gmail.com>
2020-07-08 11:09:55 -0700

    start of project

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51H2fW6IvOAiB52YKdvXcGEAqpCG8HsYsYjZrAu7V8iGEqkfcVvVkQn7LSfAWJTEs60JJKHzGuEXhX1a8pr8jokNn00zVLRJXH0';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='ALR 141'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
